9da23fb38d82a2fbc0eb55a3f313254af5543703
Ellioth <elliot22rx7@gmail.com>
2020-05-08 10:29:20 -0600

    arreglado problema de sintaxis
    agregue, de manera comentada, el ciclo que debería de mostrar el numero de imagen a comparar que mas se parece a alguna de las 360 en la base de datos

--- Tarea 1/parte3/parte3.m
+++ Tarea 1/parte3/parte3.m
@@ -6,48 +6,73 @@ N=92;
 
 %creando los valores base para el reconocimiento de rostros
 [S,f,A,U,Xi]=faceRecognition(Timages,I,J,M,N);
+
+e0 = 50;
+e1 = 15;
+
+%intentando buscar match con caras de carpeta a comparar
+testImgs=zeros(M,N,I);
+for i=1:I
+  tt="";
+  %formamos el nombre del archivo
+  tt=strcat('Comparar\',int2str(i),'_10.png');
+  %insertando las imagenes a las imagenes de prueba
+  testImgs(:,:,I)=im2double(imread(tt));
+endfor
+
+%matriz para los Xs de las imagenes de prueba
+[fila,columna]=size(U(:,:,1:rank(A(:,:,1)))'*(testImgs(:,:,1)-f))
+X=zeros(fila,columna,I);
+%matriz para los f projection de las imagenes de prueba
+[fila,columna]=size(U(:,:,1:rank(A(:,:,1)))*X(:,:,1))
+fp=zeros(fila,columna);
+r=rank(A(:,:,1));
+
+%%buscando para la imagen 1
+fProm=testImgs(:,:,1)-f;
+X(:,:,1)=U(:,:,1:r)'*(fProm);
+fp=U(:,:,1:r)*X;
+
+%para saber si la imagen es una cara, o 
+%tiene caracteristica de tal
+ef=max(max(((fProm-fp)'*(fProm-fp))^(0.5)));
+t1=10000;
+if (ef>e1)
+    disp("not a face!!")
+else
+    ei=zeros(Timages);
+    for k=1:360
+      ei(k)=max(max(((X(:,:,1)-Xi(:,:,k))'*(X(:,:,1)-Xi(:,:,k)))^(0.5)));
+      if and(ei(k)<e0,ei(k)<t1)
+        disp("a posible match!!")
+        t1=ei(k);
+      endif
+    endfor
+endif
+tt
+
+
+##for i=1:40
+##  %%buscando para la imagen 1
+##  fProm=testImgs(:,:,i)-f;
+##  X(:,:,i)=U(:,:,1:r)'*(fProm);
+##  fp=U(:,:,1:r)*X;
 ##
-##e0 = 50;
-##e1 = 15;
-##
-##%intentando buscar match con caras de carpeta a comparar
-##testImgs=zeros(M,N,I);
-##for i=1:I
-##  tt="";
-##  %formamos el nombre del archivo
-##  tt=strcat('Comparar\',int2str(i),'_10.png');
-##  %insertando las imagenes a las imagenes de prueba
-##  testImgs(:,:,I)=im2double(imread(tt));
+##  %para saber si la imagen es una cara, o 
+##  %tiene caracteristica de tal
+##  ef=max(max(((fProm-fp)'*(fProm-fp))^(0.5)));
+##  t1=10000;
+##  if (ef>e1)
+##      disp("not a face!!")
+##  else
+##      ei=zeros(Timages);
+##      for k=1:360
+##        ei(k)=max(max(((X(:,:,1)-Xi(:,:,k))'*(X(:,:,1)-Xi(:,:,k)))^(0.5)));
+##        if and(ei(k)<e0,ei(k)<t1)
+##          disp("a posible match!!")
+##          t1=ei(k);
+##        endif
+##      endfor
+##  endif
+##  tt
 ##endfor
-##
-##%matriz para los Xs de las imagenes de prueba
-##[fila,columna]=size(U(:,:,1:rank(A(:,:,1)))'*(testImgs(:,:,1)-f))
-##X=zeros(fila,columna,I);
-##%matriz para los f projection de las imagenes de prueba
-##[fila,columna]=size(U(:,:,1:rank(A(:,:,1)))*X(:,:,1))
-##fp=zeros(fila,columna);
-##
-##%%buscando para la imagen 1
-##r=rank(A(:,:,pos));
-##fProm=testImgs(:,:,1)-f;
-##X(:,:,I)=U(:,:,1:r)'*(fProm);
-##fp=U(:,:,1:r)'*X;
-##
-##%para saber si la imagen es una cara, o 
-##%tiene caracteristica de tal
-##ef=max(max(((fProm-fp)'*(fProm-fp))^(0.5)));
-##t1=10000;
-##if (ef>e1)
-##    disp("not a face!!")
-##else
-##    ei=zeros(Timages);
-##    for k=1:360
-##      ei(k)=max(max(((X(:,:,1)-Xi(:,:,k))'*(X(:,:,1)-Xi(:,:,k)))^(0.5)));
-##      if and(ei(k)<e0,ei(k)<t1)
-##        disp("a posible match!!")
-##        t1=ei(k);
-##      endif
-##    endfor
-##endif
-##
-##tt
