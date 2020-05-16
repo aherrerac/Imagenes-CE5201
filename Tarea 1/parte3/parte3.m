55e066e94e73354bb794d1ca71cb9f6dd11ac76d
Ellioth <elliot22rx7@gmail.com>
2020-05-16 13:36:16 -0600

    funciona
    se le agregaron varios cambios, ya no usa tanta potencia comunicacional, for -> n^2  -> n, se siguio mucho de lo que hizo andre, pero se le hizo el cambio de no promediar las caras, sino que usar todas, esto fue para el training, la parte 3 se adapto a estos cambios.

--- Tarea 1/parte3/parte3.m
+++ Tarea 1/parte3/parte3.m
@@ -1,80 +1,71 @@
-Timages=360;
-I=40;
-J=9;
-M=112;
-N=92;
+clc; clear;
+pkg load image
 
-%creando los valores base para el reconocimiento de rostros
-[S,f,A,U,Xi]=faceRecognition(Timages,I,J,M,N);
+%creando los valores base para el 
+%reconocimiento de rostros
+[S,f,r,U,Xi]=training();
 
-e0 = 50;
-e1 = 15;
+%definiendo e_0 y e_1
+e0 = 30;
+e1 = 10;
 
-%intentando buscar match con caras de carpeta a comparar
-testImgs=im2double(imread('Comparar\1_10.png'));
-##testImgs=zeros(M,N,I);
-##for i=1:I
-##  tt="";
-##  %formamos el nombre del archivo
-##  tt=strcat('Comparar\',int2str(i),'_10.png');
-##  %insertando las imagenes a las imagenes de prueba
-##  testImgs(:,:,I)=im2double(imread(tt));
-##endfor
+%obtiene todos los nombres de la 
+%carpeta de comparar.
+files = glob('.\Comparar\*.png');
+test=imread(char(files(1,1)));
+[M,N]=size(test);
+Timages=size(files,1);
 
-##%matriz para los Xs de las imagenes de prueba
-##[fila,columna]=size(U(:,:,1:rank(A(:,:,1)))'*(testImgs(:,:,1)-f))
-[fila,columna]=size(U(:,:,1:rank(A(:,:,1)))'*(testImgs-f))
-X=zeros(fila,columna,1);
-%matriz para los f projection de las imagenes de prueba
-[fila,columna]=size(U(:,:,1:rank(A(:,:,1)))*X(:,:,1))
-fp=zeros(fila,columna);
-r=rank(A(:,:,1));
+##mostrando la cara promedio de la base
+## de datos de caras.
+figure('Name','Imagen Promedio');
+imshow(reshape(f,[M,N]));
 
-%%buscando para la imagen 1
-fProm=testImgs(:,:,1)-f;
-X(:,:,1)=U(:,:,1:r)'*(fProm);
-fp=U(:,:,1:r)*X;
+%peparando para mostrar la cara mas parecida
+%de la base de datos, contra las de comparar.
+for i=1:Timages
+  testImg=im2single(imread(char(files(i,1))));
+  fProm=testImg(:)-f;
+  X=U(:,1:r)'*(fProm);
+  fp=U(:,1:r)*X;
+  ef=((fProm-fp)'*(fProm-fp))^0.5;
 
-%para saber si la imagen es una cara, o 
-%tiene caracteristica de tal
-ef=max(max(((fProm-fp)'*(fProm-fp))^(0.5)));
-t1=10000;
-if (ef>e1)
-    disp("not a face!!")
-else
-    ei=zeros(Timages);
-    for k=1:360
-      ei(k)=max(max(((X(:,:,1)-Xi(:,:,k))'*(X(:,:,1)-Xi(:,:,k)))^(0.5)));
-      if and(ei(k)<e0,ei(k)<t1)
-        disp("a posible match!!")
-        t1=ei(k);
-      endif
-    endfor
-endif
-tt
-
-
-##for i=1:40
-##  %%buscando para la imagen 1
-##  fProm=testImgs(:,:,i)-f;
-##  X(:,:,i)=U(:,:,1:r)'*(fProm);
-##  fp=U(:,:,1:r)*X;
-##
-##  %para saber si la imagen es una cara, o 
-##  %tiene caracteristica de tal
-##  ef=max(max(((fProm-fp)'*(fProm-fp))^(0.5)));
-##  t1=10000;
-##  if (ef>e1)
-##      disp("not a face!!")
-##  else
-##      ei=zeros(Timages);
-##      for k=1:360
-##        ei(k)=max(max(((X(:,:,1)-Xi(:,:,k))'*(X(:,:,1)-Xi(:,:,k)))^(0.5)));
-##        if and(ei(k)<e0,ei(k)<t1)
-##          disp("a posible match!!")
-##          t1=ei(k);
-##        endif
-##      endfor
-##  endif
-##  tt
-##endfor
+  if (ef>e1)
+    %mostrando rostro que no es humano
+    char(files(i,1))
+    disp("Not a human face");
+    figure('Name','Reconocimento Facial');
+    subplot(1,1,1);
+    imshow(testImg);
+    title('Not a human face');
+    continue;
+  endif
+  
+  l=size(Xi,2);
+  e_i=zeros(l,1);
+  for k=1:l
+    e_i(k,1)=((X-Xi(:,k))'*(X-Xi(:,k)))^0.5;
+  endfor
+  
+  [ei_min pos]=min(e_i);
+  
+  if (ei_min>e0)
+    %mostrando rostro humano no conocido
+    char(files(i,1))
+    disp("Not a know human face");
+    figure('Name','Reconocimento Facial');
+    subplot(1,1,1);
+    imshow(testImg);
+    title('Not a human face');
+    continue;
+  endif
+  %mostrando la cara mas parecida.
+  figure('Name','Reconocimento Facial');
+  subplot(1,2,1);
+  imshow(testImg);
+  title('Testing Face');
+  
+  subplot(1,2,2);
+  imshow(reshape(S(:,pos),[M,N]));
+  title('Most similar from DB');
+endfor
